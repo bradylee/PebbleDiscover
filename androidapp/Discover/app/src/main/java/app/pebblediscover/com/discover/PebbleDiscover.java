18481483c5cfcc62ca209e7baebb208ed2273cd5
Tigist Diriba <tgdiriba@gmail.com>
2015-04-19 11:57:24 -0500

    need to fix network error in android app

--- androidapp/Discover/app/src/main/java/app/pebblediscover/com/discover/PebbleDiscover.java
+++ androidapp/Discover/app/src/main/java/app/pebblediscover/com/discover/PebbleDiscover.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.getpebble.android.kit.PebbleKit;
@@ -18,12 +19,18 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.UUID;
 
 
@@ -37,10 +44,6 @@ public class PebbleDiscover extends Activity {
     private LocationListener locationListener;
     private Location locationTrack;
     private String username;
-
-    class Place {
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,21 +68,156 @@ public class PebbleDiscover extends Activity {
                             String encodedLatitude = URLEncoder.encode(Double.toString(location.getLatitude()), "UTF-8");
                             String encodedLongitude = URLEncoder.encode(Double.toString(location.getLongitude()), "UTF-8");
 
-                            HttpClient Client = new DefaultHttpClient();
-                            String URL = "http://104.236.213.197?username=" + username + "&latitude=" + location.getLatitude() + "&longitude=" + location.getLongitude();
+                            ArrayList<Double> inputs = new ArrayList<>();
+                            inputs.add(location.getLatitude());
+                            inputs.add(location.getLongitude());
 
-                            // Create Request to server and get response
-                            HttpGet httpget = new HttpGet(URL);
-                            HttpResponse httpResponse = Client.execute(httpget);
-                            String json_string = EntityUtils.toString(httpResponse.getEntity());
-                            JSONObject jobj = new JSONObject(json_string);
+                            AsyncTask net_task = new AsyncTask<ArrayList<Double>, Void, JSONObject>() {
+
+                                @Override
+                                protected JSONObject doInBackground(ArrayList<Double>... l) {
+                                    HttpClient Client = new DefaultHttpClient();
+                                    String URL = "http://104.236.213.197?username=" + username + "&latitude=" + l[0] + "&longitude=" + l[1];
+
+                                    // Create Request to server and get response
+                                    try {
+                                        HttpGet httpget = new HttpGet(URL);
+                                        HttpResponse httpResponse = Client.execute(httpget);
+                                        String json_string = EntityUtils.toString(httpResponse.getEntity());
+                                        return new JSONObject(json_string);
+                                    }
+                                    catch(Exception e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    return new JSONObject();
+                                }
+
+
+
+                            };
+
+                            JSONObject jobj = (JSONObject)net_task.execute(inputs).get();
 
                             // Send data to the pebble
-                            PebbleDictionary dict = new PebbleDictionary();
+                            /*PebbleDictionary dict = new PebbleDictionary();
                             dict.fromJson(json_string);
-                            PebbleKit.sendDataToPebble(getApplicationContext(), UUID.fromString("2fc99a5d-ee35-4057-aa9b-0d4dd8e35ef5"), dict);
+                            PebbleKit.sendDataToPebble(getApplicationContext(), UUID.fromString("5f306ed8-575b-48b2-9a7c-ba7b861b587f"), dict);*/
+
+                            LinearLayout foods_table = (LinearLayout) findViewById(R.id.food_table);
+                            LinearLayout stores_table = (LinearLayout) findViewById(R.id.stores_table);
+                            LinearLayout venues_table = (LinearLayout) findViewById(R.id.venues_table);
+                            LinearLayout public_transport_table = (LinearLayout) findViewById(R.id.public_transport_table);
+
+
+                            foods_table.removeAllViews();
+
+                            Iterator<?> keys = jobj.keys();
+                            while( keys.hasNext() ) {
+                                String key = (String)keys.next();
+                                if ( jobj.get(key) instanceof JSONObject ) {
+                                    JSONObject results = ((JSONObject) jobj.get(key)).optJSONObject("results");
+                                    JSONArray types = ((JSONArray)results.get("types"));
+                                    String type = (String)types.get(0);
+
+                                    if(type.equals("food")) {
+                                        // Check if name exists
+                                        String name = ((String)results.get("name"));
+                                        if(name != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Name: " + name);
+                                            foods_table.addView(tv);
+                                        }
+
+                                        String price_level = ((String)results.get("price_level"));
+                                        if(price_level != null) {
+                                            // Check if price level exists
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Price Level: " + price_level);
+                                        }
+
+                                        String rating = ((String)results.get("rating"));
+                                        if(rating != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Rating: " + rating);
+                                            foods_table.addView(tv);
+                                        }
+                                    }
+                                    else if(type.equals("store")) {
+                                        String name = ((String)results.get("name"));
+                                        if(name != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Name: " + name);
+                                            foods_table.addView(tv);
+                                        }
+
+                                        String price_level = ((String)results.get("price_level"));
+                                        if(price_level != null) {
+                                            // Check if price level exists
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Price Level: " + price_level);
+                                        }
+
+                                        String rating = ((String)results.get("rating"));
+                                        if(rating != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Rating: " + rating);
+                                            foods_table.addView(tv);
+                                        }
+                                    }
+                                    else if(type.equals("venues")) {
+                                        String name = ((String)results.get("name"));
+                                        if(name != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Name: " + name);
+                                            foods_table.addView(tv);
+                                        }
+
+                                        String price_level = ((String)results.get("price_level"));
+                                        if(price_level != null) {
+                                            // Check if price level exists
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Price Level: " + price_level);
+                                        }
+
+                                        String rating = ((String)results.get("rating"));
+                                        if(rating != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Rating: " + rating);
+                                            foods_table.addView(tv);
+                                        }
+                                    }
+                                    else if(type.equals("public_transport")) {
+                                        String name = ((String)results.get("name"));
+                                        if(name != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Name: " + name);
+                                            foods_table.addView(tv);
+                                        }
+
+                                        String price_level = ((String)results.get("price_level"));
+                                        if(price_level != null) {
+                                            // Check if price level exists
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Price Level: " + price_level);
+                                        }
+
+                                        String rating = ((String)results.get("rating"));
+                                        if(rating != null) {
+                                            TextView tv = new TextView(getApplicationContext());
+                                            tv.setText("Rating: " + rating);
+                                            foods_table.addView(tv);
+                                        }
+                                    }
+                                }
+                            }
+
                         } catch (Exception ex) {
-                            // Could not proceed with request
+                            StringWriter sw = new StringWriter();
+                            PrintWriter pw = new PrintWriter(sw);
+                            ex.printStackTrace(pw);
+                            sw.toString();
+                            Log.d("Location", "Error parsing the location"+sw);
                         }
                     }
                 }
--- androidapp/Discover/app/src/main/java/app/pebblediscover/com/discover/Registration.java
+++ androidapp/Discover/app/src/main/java/app/pebblediscover/com/discover/Registration.java
@@ -2,11 +2,14 @@ package app.pebblediscover.com.discover;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -15,10 +18,13 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,17 +34,10 @@ import java.util.HashMap;
  */
 public class Registration extends Activity {
 
-    private EditText username_input;
-    private ViewGroup foods;
-    private ViewGroup stores;
-    private ViewGroup venues;
-    private ViewGroup public_transport;
-
     @Override
-    public void onCreate(Bundle savedInstances) {
+    protected void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
-
-        username_input = (EditText) findViewById(R.id.username_input);
+        setContentView(R.layout.activity_registration);
     }
 
     public void processUsername(View v) {
@@ -46,53 +45,121 @@ public class Registration extends Activity {
         {
             JSONObject jobj = new JSONObject();
 
-            String username = username_input.getText().toString();
+            EditText usernn_input = (EditText) findViewById(R.id.usernn_input);
+            LinearLayout foods = (LinearLayout) findViewById(R.id.food);
+            LinearLayout stores = (LinearLayout) findViewById(R.id.stores);
+            LinearLayout venues = (LinearLayout) findViewById(R.id.venues);
+            LinearLayout public_transport = (LinearLayout) findViewById(R.id.public_transport);
 
+            Log.d("processUsername", "Reading in the username");
+            String username = ((EditText)findViewById(R.id.usernn_input)).getText().toString();
+            Log.d("processUsername", username);
+
+            Log.d("processUsername", "Storing the username into jobj");
             jobj.put("username", username);
             HashMap<String, ArrayList<String>> preferences = new HashMap<String, ArrayList<String>>();
 
+            Log.d("processUsername", "Storing foods into preferences");
             ArrayList<String> food_list = new ArrayList<String>();
             for(int i = 0; i < foods.getChildCount(); i++) {
-                food_list.add(((CheckBox)foods.getChildAt(i)).getText().toString());
+                CheckBox tc = ((CheckBox)foods.getChildAt(i));
+                if(tc.isChecked()) {
+                    food_list.add(tc.getText().toString());
+                }
             }
+            Log.d("processUsername", "Storing foods into preferences");
             preferences.put("food", food_list);
 
+            Log.d("processUsername", "Storing stores into preferences");
             ArrayList<String> stores_list = new ArrayList<String>();
             for(int i = 0; i < stores.getChildCount(); i++) {
-                stores_list.add(((CheckBox)stores.getChildAt(i)).getText().toString());
+                CheckBox tc = ((CheckBox)stores.getChildAt(i));
+                if(tc.isChecked()) {
+                    stores_list.add(tc.getText().toString());
+                }
             }
             preferences.put("stores", stores_list);
 
+            Log.d("processUsername", "Storing venues into preferences");
             ArrayList<String> venues_list = new ArrayList<String>();
             for(int i = 0; i < venues.getChildCount(); i++) {
-                food_list.add(((CheckBox)venues.getChildAt(i)).getText().toString());
+                CheckBox tc = ((CheckBox)venues.getChildAt(i));
+                if(tc.isChecked()) {
+                    venues_list.add(tc.getText().toString());
+                }
             }
             preferences.put("venues", venues_list);
 
+            Log.d("processUsername", "Storing public transport into preferences");
             ArrayList<String> public_transport_list = new ArrayList<String>();
             for(int i = 0; i < public_transport.getChildCount(); i++) {
-                food_list.add(((CheckBox)public_transport.getChildAt(i)).getText().toString());
+                CheckBox tc = ((CheckBox)public_transport.getChildAt(i));
+                if(tc.isChecked()) {
+                    public_transport_list.add(tc.getText().toString());
+                }
             }
             preferences.put("public_transport", public_transport_list);
 
-            jobj.put("preferences", preferences);
+            Log.d("processUsername", "Storing preferences into jobj");
+            // jobj.put("preferences", preferences);
 
-            HttpClient Client = new DefaultHttpClient();
-            String URL = "http://104.236.213.197/register";
 
+            Log.d("processUsername", "");
             // Create Request to server and get response
-            HttpPost httppost = new HttpPost(URL);
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-            httppost.setEntity(new ByteArrayEntity(jobj.toString().getBytes("UTF8")));
-            String httpResponse = Client.execute(httppost, responseHandler);
 
+            AsyncTask net_task = new AsyncTask<JSONObject, Void, Void>() {
+
+                @Override
+                protected Void doInBackground(JSONObject... v) {
+                    Log.d("processUsername", "Starting HttpClient");
+                    DefaultHttpClient Client = new DefaultHttpClient();
+                    String URL = "http://104.236.213.197/register";
+
+                    HttpPost httppost = new HttpPost(URL);
+                    try {
+                        StringEntity se = new StringEntity(v.toString());
+
+                        //sets the post request as the resulting string
+                        httppost.setEntity(se);
+                        //sets a request header so the page receving the request
+                        //will know what to do with it
+                        httppost.setHeader("Accept", "application/json");
+                        httppost.setHeader("Content-type", "application/json");
+
+                        //Handles what is returned from the page
+                        ResponseHandler responseHandler = new BasicResponseHandler();
+                        Client.execute(httppost, responseHandler);
+                        return null;
+                    }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+            };
+
+            // Wait until allocation is complete
+            try {
+                net_task.execute(new ByteArrayEntity(jobj.toString().getBytes("UTF8")));
+                net_task.get();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            Log.d("processUsername", "Calling the intent");
             Intent i = new Intent(this, PebbleDiscover.class);
             i.putExtra("username", username);
             startActivity(i);
         }
         catch(Exception ex)
         {
-            // Could not proceed with request
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            sw.toString();
+            Log.d("processUsername", "Could not successfully process the username. "+sw);
         }
     }
 
